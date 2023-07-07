9fad94d60dc5f61b5ee5812802b6c3139839f28b
Anis <107315954+ananito@users.noreply.github.com>
2023-07-07 23:22:04 +0300

    Add Logic for login, registration and logout functions

--- videoshare/urls.py
+++ videoshare/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("logout", views.logout_view, name="logout"),
+    path("login/", views.login_view, name="login")
 ]
--- videoshare/views.py
+++ videoshare/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .forms import UserForm
+from .forms import UserRegistrationForm, LoginForm
 
 # Create your views here.
 
@@ -14,29 +14,55 @@ def index(request):
     return render(request, "videoshare/index.html")
 
 def register(request):
+    if request.method == "POST":
 
-   if request.method == "POST":
+        # Send Request data to the form
+        form = UserRegistrationForm(request.POST)
 
-    # Send Request to the form
-    form = UserForm(request.POST)
-
-    try:
         if form.is_valid():
-
             user = form.save()
-    except ValidationError as e:
+            login(request, user)        
+
+        return HttpResponseRedirect(reverse("index"))
+
+    else:
+        if request.user.is_authenticated:
+            logout(request)
 
         return render(request, "videoshare/register.html", {
-        "form": UserForm(),
-        "message": e
-       }) 
+        "form": UserRegistrationForm(),
+        }) 
 
+def logout_view(request):
 
-    login(request, user)
+    logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
-   elif request.method == "GET":
-       return render(request, "videoshare/register.html", {
-        "form": UserForm(),
-       }) 
+def login_view(request):
+    if request.method == "POST":
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            
+            # Attempt to sign user in
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+
+            # Check if authentication successful
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                return render(request, "videoshare/login.html")
+    else:
+        # The users shouldnt be logged in
+        if request.user.is_authenticated:
+            logout(request)
+        
+        form = LoginForm()
+        return render(request, "videoshare/login.html", {
+            "form": form
+        })
